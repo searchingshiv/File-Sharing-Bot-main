a58b3db6ead9fa4f07c98b4855cc41641624a52e
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-17 16:07:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,8 +83,9 @@ async def start_command(client: Client, message: Message):
                 g= await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)            
             except:
                 pass
-            await asyncio.sleep(15)
-            await k.delete()
+            for k!=0:
+                await asyncio.sleep(15)
+                await k.delete()
         return
     else:
         reply_markup = InlineKeyboardMarkup(
