36c4ed61ebf67489e2b1071975e5d20e447dad96
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-18 23:10:15 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -9,6 +9,24 @@ from config import FORCE_SUB_CHANNEL, ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
+# (c) @AbirHasan2005
+
+from base64 import standard_b64encode, standard_b64decode
+
+
+def str_to_b64(__str: str) -> str:
+    str_bytes = __str.encode('ascii')
+    bytes_b64 = standard_b64encode(str_bytes)
+    b64 = bytes_b64.decode('ascii')
+    return b64
+
+
+def b64_to_str(b64: str) -> str:
+    bytes_b64 = b64.encode('ascii')
+    bytes_str = standard_b64decode(bytes_b64)
+    __str = bytes_str.decode('ascii')
+    return __str
+
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         return True
