7bdc8d245eb14761fe0dab099a03d86cbe4054df
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-19 00:58:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,7 +83,6 @@ async def start_command(client: Client, message: Message):
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
                 pass
-        return
         delay=60
         async def delete_after_delay(msg, delay):
             await asyncio.sleep(delay)
