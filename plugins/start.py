218222804944fd33b3bfa68c1d7fc8201649ca65
searchingshiv <108224889+searchingshiv@users.noreply.github.com>
2023-12-24 19:43:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -104,9 +104,14 @@ async def start_command(client: Client, message: Message):
             reply_markup = reply_markup,
             disable_web_page_preview = True,
             quote = True
-        )
-        return
-
+            
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
+    sent_message = await media_forward(bot, user_id, file_id)
+    await reply_forward(message=sent_message, file_id=file_id)
+    asyncio.create_task(delete_after_delay(sent_message, 30))
+async def delete_after_delay(message, 30):
+    await asyncio.sleep(30)
+    await message.delete()
     
 #=====================================================================================##
 
